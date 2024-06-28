d861abc6ce4db708ba044911a4009f119b843c1c
CompanyUser <1101642762@qq.com>
2024-06-28 17:58:52 +0800

    基于rabbitMQ的多线程实现,调整工作线程数

--- src/main/java/com/example/opencvforjava/Config/RabbitMQConfig.java
+++ src/main/java/com/example/opencvforjava/Config/RabbitMQConfig.java
@@ -2,7 +2,6 @@ package com.example.opencvforjava.Config;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,9 +32,12 @@ public class RabbitMQConfig {
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        //获取当前系统cpu核心数
+        int cpuCoreSize = Runtime.getRuntime().availableProcessors();
+        System.out.println("当前设备的CPU核心数为:"+cpuCoreSize);
         factory.setConnectionFactory(connectionFactory);
-        factory.setConcurrentConsumers(5); // 设置同时处理消息的消费者数量
-        factory.setMaxConcurrentConsumers(10); // 设置最大并发消费者数量
+        factory.setConcurrentConsumers(cpuCoreSize + 1); // 设置同时处理消息的消费者数量
+        factory.setMaxConcurrentConsumers(cpuCoreSize * 2); // 设置最大并发消费者数量
         // 如果需要，可以配置其他属性，例如消费者标签策略、事务管理等
         // factory.setConsumerTagStrategy(...);
         // factory.setTransactionManager(...);
